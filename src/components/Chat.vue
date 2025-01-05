35f90521f181fcb1b9984ead7d2578e584e449a3
degiam <degiam@ymail.com>
2025-01-05 14:43:08 +0700

    update: copyright

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -415,7 +415,7 @@ onMounted(() => {
 
 <template>
   <div class="flex justify-center items-center min-h-screen p-6 main-layout">
-    <div class="w-full max-w-lg mb-20">
+    <div class="w-full max-w-lg mb-10 md:mb-16">
 
       <div class="w-fit mx-auto mb-4">
         <Brand />
@@ -507,10 +507,15 @@ onMounted(() => {
         <Button type="submit" severity="primary" label="Chat Sekarang" class="rounded-lg text-white border-cyan-500 bg-cyan-500 hover:bg-cyan-600 dark:bg-cyan-600 dark:hover:bg-cyan-500 mt-4" />
       </Form>
 
+      <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1 mt-14">
+        <span>Built on</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+          <path fill="#42B883" d="M14.6,2.5L12,6.9L9.4,2.5H0.9L12,21.5L23.1,2.5H14.6z" />
+          <path fill="#35495E" d="M14.6,2.5L12,6.9L9.4,2.5H5.3L12,13.9l6.7-11.4H14.6z" />
+        </svg>
+        <span>Vue</span>
+      </div>
+
     </div>
   </div>
 </template>
-
-<style scoped>
-/* Add any required scoped styles here */
-</style>
