ca0c71c12631b0e55f3c015ea0ac6f7fff0f7de5
degiam <degiam@ymail.com>
2025-01-06 10:37:48 +0700

    refactory code

---
+++ src/components/Built.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1 mt-14">
+    <span>Built on</span>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+      <path fill="#42B883" d="M14.6,2.5L12,6.9L9.4,2.5H0.9L12,21.5L23.1,2.5H14.6z" />
+      <path fill="#35495E" d="M14.6,2.5L12,6.9L9.4,2.5H5.3L12,13.9l6.7-11.4H14.6z" />
+    </svg>
+    <span>Vue</span>
+  </div>
+</template>
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -11,6 +11,7 @@ import Message from 'primevue/message';
 import WhatsAppLogo from '../assets/brand-whatsapp.svg';
 import TelegramLogo from '../assets/brand-telegram.svg';
 import Brand from './Brand.vue';
+import Built from './Built.vue';
 
 type Platform = {
   name: string,
@@ -507,14 +508,7 @@ onMounted(() => {
         <Button type="submit" severity="primary" label="Chat Sekarang" class="rounded-lg text-white border-cyan-500 bg-cyan-500 hover:bg-cyan-600 dark:bg-cyan-600 dark:hover:bg-cyan-500 mt-4" />
       </Form>
 
-      <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1 mt-14">
-        <span>Built on</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-          <path fill="#42B883" d="M14.6,2.5L12,6.9L9.4,2.5H0.9L12,21.5L23.1,2.5H14.6z" />
-          <path fill="#35495E" d="M14.6,2.5L12,6.9L9.4,2.5H5.3L12,13.9l6.7-11.4H14.6z" />
-        </svg>
-        <span>Vue</span>
-      </div>
+      <Built />
 
     </div>
   </div>
