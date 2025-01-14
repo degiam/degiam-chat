2acc5ec56262bc7248d6940b56f45c54ceb5ad2d
degiam <degiam@ymail.com>
2025-01-14 16:16:41 +0700

    update: built

--- src/components/Built.vue
+++ src/components/Built.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1 mt-14">
-    <span>Built on</span>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-      <path fill="#42B883" d="M14.6,2.5L12,6.9L9.4,2.5H0.9L12,21.5L23.1,2.5H14.6z" />
-      <path fill="#35495E" d="M14.6,2.5L12,6.9L9.4,2.5H5.3L12,13.9l6.7-11.4H14.6z" />
-    </svg>
-    <span>Vue</span>
-  </div>
+  <section class="text-xs flex items-center justify-center flex-col gap-2 mt-14">
+    <div class="text-slate-400/60 dark:text-slate-600 text-xs flex items-center justify-center gap-1">
+      <span>Built on</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+        <path fill="#42B883" d="M14.6,2.5L12,6.9L9.4,2.5H0.9L12,21.5L23.1,2.5H14.6z" />
+        <path fill="#35495E" d="M14.6,2.5L12,6.9L9.4,2.5H5.3L12,13.9l6.7-11.4H14.6z" />
+      </svg>
+      <span>Vue</span>
+    </div>
+
+    <div class="text-slate-400/60 dark:text-slate-600 hover:text-black dark:hover:text-white transition underline hover:no-underline">
+      <a href="https://github.com/degiam/degiam-chat" target="_blank" rel="noopener noreferrer">Fork at GitHub</a>
+    </div>
+  </section>
 </template>
